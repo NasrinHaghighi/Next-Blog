29f2d1aada24db4f91e815aef493c847730d8001
nasrin haghighi <ns.haghighi@gmail.com>
2023-06-27 08:11:26 +0100

    feat:tailwind typography plugin

--- frontend/src/pages/posts/[hashId]/[postSlug].js
+++ frontend/src/pages/posts/[hashId]/[postSlug].js
@@ -7,7 +7,7 @@ import {LuSave} from 'react-icons/lu'
 function PostDetail({postDetail}) {
     //console.log(postDetail)
   return (
-    <div className='conatiner  mx-auto lg:max-w-screen-2xl px-4 py-4 md:px-2'>
+    <div className='conatiner  mx-auto lg:max-w-screen-xl  px-5 py-5 md:px-2'>
         <div className='flex flex-col md:flex-row items-center justify-between'>
          <div className='flex flex-row justify-start items-center'>
 <div className='w-20 h-20 rounded-full bg-red-600 ml-6'>
@@ -32,6 +32,56 @@ function PostDetail({postDetail}) {
         </div>
          </div>
         </div>
+        <main
+            className="prose prose-xl prose-slate prose-h1:text-xl md:prose-h1:text-3xl  prose-h1:font-black prose-h2:text-xl md:prose-h2:text-2xl prose-h2:font-extrabold prose-p:text-base prose-p:leading-8 md:prose-p:text-lg md:prose-p:leading-10 
+            prose-img:rounded-xl prose-img:m-auto prose-a:text-red-500 mb-8 max-w-screen-xl  mx-auto py-8
+        "
+          >
+            <h1>عنوان اصلی...</h1>
+            <h2>عنوان اول تستی</h2>
+            <p>
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
+              گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای
+              شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+              باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان
+              را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+              خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+              دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+              حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد
+              استفاده قرار گیرد.
+            </p>
+          <img src='/images/Vue.png' alt="" width={500} height={500} /> 
+            <h2>عنوان تستی دوم </h2>
+            <p>
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان
+              گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای
+              شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+              باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان
+              را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+              خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و
+              دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+              حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد
+              استفاده قرار گیرد.
+            </p>
+            <h2>نحوه کانفیگ تیلویند</h2>
+            <p>
+              بدون استفاده از <a href="https://highlightjs.org/">highlight.js</a> میتوان به سادگی
+              کدها را داخل محتوای بلاگ ها قرار داد!
+            </p>
+            <p>
+              به عنوان مثال، برای کانفیگ تیلویند باید از فایل <code>tailwind.config.js</code>{" "}
+              استفاده کرد:
+            </p>
+            <pre dir="ltr">{`module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+`}</pre>
+          </main>
     </div>
   )
 }
